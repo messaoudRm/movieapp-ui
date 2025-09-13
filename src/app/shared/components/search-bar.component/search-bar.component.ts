914e4eac04dc11ef4cfe532c2fccdddebbc624c2
Messaoud Rm <messaoud.ramouche@example.com>
2025-09-13 17:35:25 +0200

    fix serach-bar and dark theme style

--- src/app/shared/components/search-bar.component/search-bar.component.ts
+++ src/app/shared/components/search-bar.component/search-bar.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatSuffix} from '@angular/material/input';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-search-bar',
   imports: [
-    MatFormField,
     MatIcon,
-    MatInput,
     MatIconButton,
-    MatLabel,
+    MatSuffix,
   ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
